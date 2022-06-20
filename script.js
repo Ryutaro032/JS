cff316e7c47e3fd078bd12bb6e1a3814c2b48ebe
Ryutaro032 <motti.k32@gmail.com>
2022-06-20 22:11:59 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let lists=document.getElementsByClassName('pull-down-list');
 console.log(lists);
+/*const pullDownButton=document.getElementById("lists");
+console.log(pullDownButton);*/
+window.addEventListener('click',function(){
+    const pullDownButton=this.document.getElementById("lists");
+    console.log(pullDownButton);
+});
