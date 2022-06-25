c00a2d17c088d69032afde17f9096c07374db7fb
Ryutaro032 <motti.k32@gmail.com>
2022-06-25 20:09:05 +0900

    インラインスタイル編

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ console.log(pullDownButton);*/
 window.addEventListener('click',function(){
     const pullDownButton=this.document.getElementById("lists");
     console.log(pullDownButton);
+    pullDownButton.addEventListener('mouseover',function(){
+        pullDownButton.setAttribute('style','background-color:blue;');
+    });
+    pullDownButton.addEventListener('mouseout',function(){
+        pullDownButton.removeAttribute('style','background-color:red;');
+    });
+    pullDownButton.addEventListener('click',function(){
+        pullDownButton.setAttribute('style','background-color:green;');
+    });
 });
