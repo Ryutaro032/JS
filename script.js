5025d2203e877011254d432035d439e254dedff3
Ryutaro032 <motti.k32@gmail.com>
2022-06-30 19:08:54 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let lists=document.getElementsByClassName('pull-down-list');
-console.log(lists);
-/*const pullDownButton=document.getElementById("lists");
-console.log(pullDownButton);*/
-window.addEventListener('click',function(){
-    const pullDownButton=this.document.getElementById("lists");
-    console.log(pullDownButton);
-    pullDownButton.addEventListener('mouseover',function(){
-        pullDownButton.setAttribute('style','background-color:blue;');
+const pullDownButton=document.getElementById("lists");
+const pullDownParents=document.getElementById("pull-down");
+    pullDownButton.addEventListener("mouseover",function(){
+        this.setAttribute('style','background-color:blue;');
     });
     pullDownButton.addEventListener('mouseout',function(){
-        pullDownButton.removeAttribute('style','background-color:red;');
+        this.removeAttribute('style','background-color:red;');
     });
     pullDownButton.addEventListener('click',function(){
-        pullDownButton.setAttribute('style','background-color:green;');
+        if(pullDownParents.getAttribute("style")=="display:block;"){
+            pullDownParents.removeAttribute("style","display:block;");
+            console.log("非表示");
+        }else{
+            pullDownParents.setAttribute("style","display:block;");
+            console.log("表示");
+        };
     });
-});
